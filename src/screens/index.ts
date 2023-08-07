b6844ab95f6cd482766b44782241d7e2dc912a50
TheTrunk <tadeas.kmenta@zel.cash>
2023-08-07 12:29:49 +0200

    remove example

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,5 +1,4 @@
 export { default as Startup } from './Startup/Startup';
-export { default as Example } from './Example/Example';
 export { default as Welcome } from './Welcome/Welcome';
 export { default as Create } from './Create/Create';
 export { default as Restore } from './Restore/Restore';
