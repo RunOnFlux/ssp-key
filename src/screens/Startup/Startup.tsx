137bf47a13714b20e911f6a69f2f5952b7acad60
TheTrunk <tadeas.kmenta@zel.cash>
2023-08-30 14:08:36 +0200

    reduce timeout

--- src/screens/Startup/Startup.tsx
+++ src/screens/Startup/Startup.tsx
@@ -13,7 +13,7 @@ const Startup = ({ navigation }: ApplicationScreenProps) => {
     await new Promise((resolve) =>
       setTimeout(() => {
         resolve(true);
-      }, 2000),
+      }, 500),
     );
     await setDefaultTheme({ theme: 'default', darkMode: null });
     navigation.reset({
