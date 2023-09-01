1f317e6227096c916b9002350fb4eb396f141ce3
TheTrunk <tadeas.kmenta@zel.cash>
2023-09-01 14:29:20 +0200

    improve script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,12 +2,9 @@
 export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 export NODE_OPTIONS=--max_old_space_size=8192
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
 # Install CocoaPods and yarn using Homebrew.
 brew install cocoapods
 brew install ruby
-brew install rails
 brew install node@18
 brew link node@18
 brew install yarn
