869f20ea62b95f824b6c0c249904e7103f7689fd
TheTrunk <tadeas.kmenta@zel.cash>
2023-09-01 09:44:40 +0200

    adjust ci scripts

---
+++ ios/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo -e "machine github.com\n  login $GH_TOKEN" > ~/.netrc
+
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
+export NODE_OPTIONS=--max_old_space_size=8192
+# Install CocoaPods and yarn using Homebrew.
+brew install cocoapods
+brew install node@18
+brew link node@18
+brew install yarn
+
+# Install dependencies
+yarn
+yarn podprod
