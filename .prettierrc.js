beb5ac9ccec015785c809599746f1b32c23ac8e7
TheTrunk <tadeas.kmenta@zel.cash>
2024-07-30 16:33:46 +0800

    update

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   arrowParens: 'always',
   bracketSameLine: false,
   singleQuote: true,
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-import { getDefaultConfig, mergeConfig } from '@react-native/metro-config';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -19,4 +20,4 @@ const config = {
   },
 };
 
-export default mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
