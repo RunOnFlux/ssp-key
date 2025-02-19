8ef1361552f9bf94a22bcf1d854a0ee124705337
TheTrunk <tadeas@runonflux.io>
2025-02-19 15:25:27 +0800

    tapjacking, overlay protection

--- android/app/src/main/java/io/runonflux/sspkey/MainActivity.kt
+++ android/app/src/main/java/io/runonflux/sspkey/MainActivity.kt
@@ -1,33 +1,61 @@
 package io.runonflux.sspkey
 
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.provider.Settings
+import android.widget.Toast
+import android.content.Context
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import android.view.ViewGroup
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "SSPKey"
+    override fun getMainComponentName(): String = "SSPKey"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
-  // disable screenshots, app overview
-  override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
-      window.setFlags(
-          WindowManager.LayoutParams.FLAG_SECURE,
-          WindowManager.LayoutParams.FLAG_SECURE
-      )
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Block screenshots & overlays
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Apply tapjacking protection
+        applyTapjackingProtection()
+
+        // Detect active overlays and warn users
+        if (isOverlayEnabled(this)) {
+            Toast.makeText(this, "Warning: Screen overlays detected! Disable them for security.", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun applyTapjackingProtection() {
+        val rootView = findViewById<View>(android.R.id.content)
+        setFilterTouchesWhenObscured(rootView)
+    }
+
+    private fun setFilterTouchesWhenObscured(view: View?) {
+        if (view is ViewGroup) {
+            for (i in 0 until view.childCount) {
+                setFilterTouchesWhenObscured(view.getChildAt(i))
+            }
+        }
+        view?.filterTouchesWhenObscured = true
+    }
+
+    private fun isOverlayEnabled(context: Context): Boolean {
+        return Settings.canDrawOverlays(context)
+    }
 }
