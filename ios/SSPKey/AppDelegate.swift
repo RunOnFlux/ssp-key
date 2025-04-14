a9dc25e84cc836961fb5221cf0cc478848e91411
TheTrunk <tadeas@runonflux.io>
2025-04-14 19:51:59 +0800

    fix

--- ios/SSPKey/AppDelegate.swift
+++ ios/SSPKey/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "RnDiffApp",
+      withModuleName: "SSPKey",
       in: window,
       launchOptions: launchOptions
     )
